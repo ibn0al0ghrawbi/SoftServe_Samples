dcae989dc2f51a08e1afca9178379f5d1fff8e67
ibn0al0ghrawbi <75629323+ibn0al0ghrawbi@users.noreply.github.com>
2022-06-10 16:30:02 +0300

    Create shortestWord.js

---
+++ shortestWord.js
@@ -0,0 +1,12 @@
+let input = "bitcoin take over the world maybe who knows perhaps";
+let inputArray = input.split(' ');
+
+let shortest = 1000000000;
+
+for(let i = 0; i<inputArray.length; i++){
+  if(inputArray[i].length < shortest){
+    shortest = inputArray[i].length;
+  }
+}
+
+console.log(shortest)
